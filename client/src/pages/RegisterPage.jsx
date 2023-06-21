65be5be1ce318d253648ca7d52e8ccb8780a9174
raiyan22 <raiyanislam058@gmail.com>
2023-06-21 19:53:51 +0600

    added states for register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,26 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-                
+
                 <h1 className="text-4xl text-center pb-5"> Register</h1>
-                
+
                 <form className="max-w-md mx-auto">
-                    <input type="text" placeholder="your name" />
-                    <input type="email" placeholder="your@email.com" />
-                    <input type="password" placeholder="password" />
+                    <input type="text" placeholder="your name"
+                        value={name}
+                        onChange={(ev) => setName(ev.target.value)} />
+
+                    <input type="email" placeholder="your@email.com"
+                        value={email}
+                        onChange={(ev) => setEmail(ev.target.value)} />
+
+                    <input type="password" placeholder="password" 
+                        value={password}
+                        onChange={(ev) => setPassword(ev.target.value)} />
+
                     <button className="primary">Login</button>
+
                     <div className="text-center py-3 text-gray-400">
-                        Already have an account? 
+                        Already have an account?
                         <Link className="font-semibold px-1 text-black" to={'/login'}>
                             Login
                         </Link>
                     </div>
+
                 </form>
-            
+
             </div>
         </div>
     );
 }
+
+// start here : https://youtu.be/MpQbwtSiZ7E?t=2731
