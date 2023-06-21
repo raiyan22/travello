0e4064ac7f69f66ab4b981d4a79465dcaf22b077
raiyan22 <raiyanislam058@gmail.com>
2023-06-21 06:25:45 +0600

    added Layout comp as template

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,10 +1,7 @@
-import { Link } from "react-router-dom";
-import Header from "../components/Header";
 
 export default function IndexPage() {
   return (
     <div>
-      <Header />
     </div>
   );
 }
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+
 export default function LoginPage() {
     return 'login page';
 }
